f16af8420214cb1e109512c68d9aa05edc465113
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-01-20 23:42:41 +0545

    Added comments

--- FirstFile.java
+++ FirstFile.java
@@ -7,12 +7,14 @@ public class FirstFile {
     public static void main(String[] args) {
         try (Scanner ob = new Scanner(System.in)) {
             try {
+                // To write in file "File1.txt"
                 FileOutputStream fout = new FileOutputStream("File1.txt");
                 System.out.println("Enter a line of text:");
                 String s = ob.nextLine();
                 byte[] b = s.getBytes();
                 fout.write(b);
                 fout.close();
+                // To read from file "File1.txt"
                 FileInputStream fin = new FileInputStream("File1.txt");
                 int ch;
                 while ((ch = fin.read()) != -1) {
