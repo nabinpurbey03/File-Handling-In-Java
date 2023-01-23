336359c4c4793729ae4762e954fb1d2ca1935df3
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-01-23 22:55:55 +0545

    Updated with comment

--- Problem.java
+++ Problem.java
@@ -7,26 +7,33 @@ public class Problem {
         String s = "This file will demostrate how to cope data from one file and paste to another file.";
         byte[] b = s.getBytes();
         try {
+            // Creating a initial file
             FileOutputStream fout = new FileOutputStream("main_file.txt");
-            fout.write(b);
-            fout.close();
-
+            fout.write(b);// Written in file
+            fout.close(); // Closed fout
+            // Creatin a file to read from the file
             FileInputStream fin = new FileInputStream("main_file.txt");
+            // Creating a file to copy data from the initial file
             FileOutputStream fout2 = new FileOutputStream("copied_file.txt");
             int ch;
+            // Writing in second file
             while ((ch = fin.read()) != -1) {
                 fout2.write(ch);
             }
+            fin.close();// fin closed
+            // Creatin a file to read from second the file
             FileInputStream fin2 = new FileInputStream("copied_file.txt");
             int ch2;
+            // Reading from second file
             while ((ch2 = fin2.read()) != -1) {
                 System.out.print((char) ch2);
             }
-            fin.close();
-            fout2.close();
-            fin2.close();
+            fout2.close();// fout2 closed
+            fin2.close();// fin2 closed
         } catch (IOException ex) {
             ex.printStackTrace();
+        } finally {
+            System.out.println("\nThe file has ran successfully.");
         }
     }
 }
