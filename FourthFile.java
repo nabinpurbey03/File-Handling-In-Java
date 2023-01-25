8f2f5a9e9b28fd78613ac9bae01e7845f852ad52
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-01-25 19:36:38 +0545

    Added documentation

--- FourthFile.java
+++ FourthFile.java
@@ -14,12 +14,24 @@ class Student implements Serializable {
     private int id;
     private String name, faculty;
 
+    /**
+     * The constructor "Student" initializes the instance variables of class
+     * "Student"
+     * 
+     * @param id      Takes the integer input as an ID of a student
+     * @param name    Takes a string input as name of a student
+     * @param faculty Takes a string input as faculty of a student
+     */
     Student(int id, String name, String faculty) {
         this.id = id;
         this.name = name;
         this.faculty = faculty;
     }
 
+    /**
+     * The method "showInfo" gives the output of the details passed in constructor's
+     * parameters
+     */
     public void showInfo() {
         System.out.println("ID = " + id);
         System.out.println("Name = " + name);
@@ -35,19 +47,19 @@ public class FourthFile {
             FileOutputStream fout = new FileOutputStream("File4.txt");
             ObjectOutputStream oos = new ObjectOutputStream(fout);
             oos.writeObject(s);
-            fout.close();
-            oos.close();
+            fout.close(); // fout closed
+            oos.close(); // oos closed
             // Deserialization
             FileInputStream fin = new FileInputStream("File4.txt");
             ObjectInputStream ois = new ObjectInputStream(fin);
             Student s1 = (Student) ois.readObject();
-            s1.showInfo();
-            fin.close();
-            ois.close();
+            s1.showInfo(); // callin the method
+            fin.close(); // fin closed
+            ois.close(); // ois closed
         } catch (IOException ex) {
             ex.printStackTrace();
         } finally {
-            System.out.println("The file has ran successfully.");
+            System.out.println("The file has run successfully.");
         }
     }
 }
